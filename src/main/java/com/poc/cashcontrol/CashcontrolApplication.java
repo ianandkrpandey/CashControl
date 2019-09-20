b37d63f364e19f094c1fea6ef01ac45dcd2e01a0
Anandkumar <anandk@Triconinfotech.com>
2019-09-20 16:09:46 +0530

    Initial Commit- Maven Project Structure

---
+++ src/main/java/com/poc/cashcontrol/CashcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.poc.cashcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashcontrolApplication.class, args);
+	}
+
+}
