2c3d974765e816807b739ce066c085b94459385f
anandk <anandk@Triconnodews103.triconhome.local>
2019-09-23 16:17:19 +0530

    Added @EnableAutoConfiguration for The Read the Controller Class

--- src/main/java/com/cashcontrol/controller/ApiController.java
+++ src/main/java/com/cashcontrol/controller/ApiController.java
@@ -1,9 +1,14 @@
 package com.cashcontrol.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/cc")
 public class ApiController {
-
+	@RequestMapping("/test/")
+	public String test() {
+		 return "ANAND";
+	}
 
 }
--- src/main/java/com/poc/cashcontrol/CashcontrolApplication.java
+++ src/main/java/com/poc/cashcontrol/CashcontrolApplication.java
@@ -1,9 +1,15 @@
 package com.poc.cashcontrol;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@Import({ com.cashcontrol.controller.ApiController.class })
 public class CashcontrolApplication {
 
 	public static void main(String[] args) {
