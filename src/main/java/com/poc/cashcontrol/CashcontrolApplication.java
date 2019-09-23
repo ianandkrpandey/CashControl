4dfc658246ce92a7344d29f13bc3bf868a486b98
anandk <anandk@Triconnodews103.triconhome.local>
2019-09-23 16:55:43 +0530

    Added Swagger Defination and application.Yaml and added All Profile into same File

--- src/main/java/com/poc/cashcontrol/CashcontrolApplication.java
+++ src/main/java/com/poc/cashcontrol/CashcontrolApplication.java
@@ -6,10 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
 @Import({ com.cashcontrol.controller.ApiController.class })
+@EnableSwagger2
 public class CashcontrolApplication {
 
 	public static void main(String[] args) {
